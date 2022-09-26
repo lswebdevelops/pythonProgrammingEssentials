e2ce674ce0426cc3bc8d62eb0ad1654e1c0dde0c
Luciano Sardanha <F0FP583@t-systems.com.br>
2022-09-26 13:32:34 -0300

    doing math using python

---
+++ number.py
@@ -0,0 +1,28 @@
+"""
+Demonstration of simple arithmetic expressions in Python
+"""
+print('unary operators')
+print(+3)
+print(-5)
+
+print( 3 + 4)
+
+print (234- 324234)
+print(3.4+3)
+
+
+print(3*99)
+
+print(3/8)
+
+print(23.3 /23.455)
+print(23.3 // 23.455)
+print(int(23.3 // 23.455))
+
+#exponentiation
+
+print(3**2)
+print(4 ** 2)
+print(5**2)
+print("9 squared: 9^1/2 = 9 ** 0.5")
+print(9 ** 0.5)
