049a51ca027e0344832271b25c63fddf33d1783d
Luciano Sardanha <F0FP583@t-systems.com.br>
2022-09-26 14:59:32 -0300

    converting feet in miles

---
+++ miles_to_feet.py
@@ -0,0 +1,20 @@
+"""
+Template - Compute the number of feet corresponding to a number of miles.
+"""
+# There are 5280 feet in a mile. Write a Python statement that calculates and prints the number of feet in 1313 miles. 
+###################################################
+# Miles to feet conversion formula
+# Student should enter statement on the next line.
+
+feet_in_a_mile = 5280
+number_of_miles = 13
+
+convert_miles_in_feet = feet_in_a_mile * number_of_miles
+print("There are" ,str(convert_miles_in_feet),"feet in" ,str(number_of_miles) ,"miles.")
+print("There are " + str(convert_miles_in_feet) + " feet in " + str(number_of_miles)  + " miles." )
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#68640
