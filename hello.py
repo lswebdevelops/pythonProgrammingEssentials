5df1854797f901b538241dcc6a234a8cdbbc0782
Luciano Sardanha <F0FP583@t-systems.com.br>
2022-09-26 11:34:12 -0300

    printing numbers and converting int into float and viceversa

--- hello.py
+++ hello.py
@@ -1,1 +1,42 @@
 print('Hello, world!')
+
+""" testing comments"""
+
+print('int')
+print("---")
+print(0)
+print(1)
+print(-3)
+
+print(70383028364830)
+print("")
+
+print('float')
+print(0.0)
+print(7.35)
+print(-43.2)
+print("")
+
+# Scientific/exponential notation
+print("Scientific notation")
+print("-------------------")
+print(5e32)
+print(999999999999999999999999999999999999999.9)
+
+print("")
+
+# Infinity
+print("Infinity")
+print("--------")
+print(1e500)
+print(-1e500)
+
+print("")
+
+# Conversions
+print("Conversions between numeric types")
+print("---------------------------------")
+
+print(float(3))
+print(int(4.1))
+print(int(4.9))
