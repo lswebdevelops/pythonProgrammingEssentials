4fa3f6c96be88cc8e09b84482cea5debac5836ae
Luciano Sardanha <F0FP583@t-systems.com.br>
2022-10-04 11:23:13 -0300

    adding some functions using if conditionals

---
+++ comparisons.py
@@ -0,0 +1,54 @@
+# demonstration of arithmetic comparisons
+
+
+# six different arthmetic comparison operators
+
+
+print("Comparisons")
+
+print("*////////////////")
+
+print(7>3)
+print(7<3)
+print(7>=3)
+print(7<=3)
+print(7==3)
+print(7!=3)
+
+print("comparing variables ")
+
+num1 = 2.4
+num2 = 34.4
+
+greater = num1 > num2
+print(greater)
+
+
+# comparing strings
+
+print("comparing strings")
+
+name = "scott"
+name2 = "John"
+
+stringComp = name == name2
+print(stringComp)
+
+# beware of = and == confusion
+
+equal = name != "Joe"
+
+print("print(Name)")
+print(name)
+print("print(name != name2)")
+
+print(name != name2)
+print('print(equal)')
+
+print(equal)
+
+test = 3
+test2 = '3'
+
+three = test == test2
+print(three)
---
+++ conditionals.py
@@ -0,0 +1,68 @@
+"""
+Demonstration of if statements.
+"""
+def greet(friend, money):
+    """
+    Greet people. Say hi if they are your friend and give
+    them $20 if you have enough money.
+    """
+    if friend:
+        print("Hi!")
+
+    if money > 20:
+        money = money -20
+
+    return money
+
+money = 250
+
+money = greet(False, money)
+print("\nMoney", money)
+print()
+
+
+money = greet(True, money)
+print("Money", money)
+print()
+
+
+money = greet(True, money)
+print("Money", money)
+print()
+
+
+money = greet(True, money)
+print("Money", money)
+print()
+
+
+
+
+money = greet(False, money)
+print("Money", money)
+print()
+
+
+money = greet(True, money)
+print("Money", money)
+print()
+
+
+
+
+money = greet(False, money)
+print("Money", money)
+print()
+
+
+
+money = greet(True, money)
+print("Money", money)
+print()
+
+
+
+
+money = greet(False, money)
+print("Money", money)
+print()
